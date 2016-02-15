51f29441a7d38c00b58952c0ee900e7b6507485a
Huy Nguyen <huy@ronin.ws>
2016-02-15 08:55:01 -0800

    Created base selenium project with references and upgraded log4net

---
+++ Abot.Selenium/TestMe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Abot.Selenium
+{
+	[TestFixture("Selenium")]
+	public class TestMe : IDisposable
+	{
+		readonly IWebDriver _w;
+
+		public TestMe()
+		{
+			_w = new FirefoxDriver();
+			_w.Navigate().GoToUrl("https://losangeles.craigslist.org/search/jjj");
+		}
+
+		[Test]
+		public void ClickLink()
+		{
+			IEnumerable<IWebElement> rows = _w.FindElements(By.CssSelector(".content .row"));
+			CollectionAssert.IsNotEmpty(rows);
+		}
+
+		[OneTimeTearDown]
+		public void Dispose()
+		{
+			_w.Quit();
+		}
+	}
+}
